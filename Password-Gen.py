9fcf2a9e2ed97083ea23ee5bcd1a9548dcc9e534
TDuck1998 <thomas.coates@laterooms.com>
2019-02-26 21:36:09 +0000

    Tweaked

--- Password-Gen.py
+++ Password-Gen.py
@@ -1,15 +1,39 @@
 import random
 
 #PASSWORD GENERATOR#
+print('''
+██████╗ ██╗    ██╗ ██████╗ ███████╗███╗   ██╗
+██╔══██╗██║    ██║██╔════╝ ██╔════╝████╗  ██║
+██████╔╝██║ █╗ ██║██║  ███╗█████╗  ██╔██╗ ██║
+██╔═══╝ ██║███╗██║██║   ██║██╔══╝  ██║╚██╗██║
+██║     ╚███╔███╔╝╚██████╔╝███████╗██║ ╚████║
+╚═╝      ╚══╝╚══╝  ╚═════╝ ╚══════╝╚═╝  ╚═══╝
+
+''')
+
 chars = '`¬!"£$%^&*()_+{}@~:>?<-=[];#./abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+print('welcome')
+input('Press ENTER to continue')
 
 choiceLen = input('Specify password length: ')
 choiceLen = int(choiceLen)
 choiceNum = input('How many passwords: ')
 choiceNum = int(choiceNum)
 
-for a in range(choiceNum):
-    password = ''
-    for a in range(choiceLen):
-        password += random.choice(chars)
-    print(password)
+print('-' * choiceLen)
+print('DISPLAYING ' + str(choiceNum) + ' PASSWORDS')
+print('-' * choiceLen)
+
+if choiceNum <= 0:
+    print('Error')
+elif choiceLen <= 0:
+    print('Error')
+else:
+    for a in range(choiceNum):
+        password = ''
+        for a in range(choiceLen):
+            password += random.choice(chars)
+        print('-' * choiceLen)
+        print(password)
+print('-' * choiceLen)
