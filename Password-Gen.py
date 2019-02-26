a3b986aa2a3cc1aaf80a6bba0d41779af911809e
TDuck1998 <thomas.coates@laterooms.com>
2019-02-26 18:39:16 +0000

    Added multiple password option

--- Password-Gen.py
+++ Password-Gen.py
@@ -1,12 +1,15 @@
 import random
 
+#PASSWORD GENERATOR#
 chars = '`¬!"£$%^&*()_+{}@~:>?<-=[];#./abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-password = ''
 
-choice = input('Specify password length: ')
-choice = int(choice)
+choiceLen = input('Specify password length: ')
+choiceLen = int(choiceLen)
+choiceNum = input('How many passwords: ')
+choiceNum = int(choiceNum)
 
-for a in range(choice):
-    password += random.choice(chars)
-
-print(password)
+for a in range(choiceNum):
+    password = ''
+    for a in range(choiceLen):
+        password += random.choice(chars)
+    print(password)
